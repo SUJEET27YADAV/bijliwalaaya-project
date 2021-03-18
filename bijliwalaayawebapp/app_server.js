b4718f6c9164ba5c570c12a668329519637d9a5c
SUJEET27YADAV <40136925+SUJEET27YADAV@users.noreply.github.com>
2021-03-19 03:04:59 +0530

    Add files via upload

---
+++ bijliwalaayawebapp/app_server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.listen(3000, ()=> {console.log('Listening port is 3000')});
+app.use(express.static('public'));
+//const server = http.createServer((req, res)=> {
+  //  if(req.url === '/'){
+    //    res.write('Hello World');
+      //  res.end();
+   // }
+//});
+// server.on('connection', (socket) => {
+//     console.log("new connection--")
+//})
+//server.listen(3000);
+
+//console.log('Listening port is 3000');
